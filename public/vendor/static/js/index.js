cee303d81335f07d322135081d28b39e1a3add31
kouji sugimoto <kouji36000@gmail.com>
2017-09-08 19:25:48 +0900

    add old-stone and new-stone

--- public/vendor/static/js/index.js
+++ public/vendor/static/js/index.js
@@ -72,6 +72,46 @@ ws.onmessage = function (evt) {
             $('.mika-time').text(getDate());
             $('#mika_tbl tbody > tr:last').after(rec);
             break;
+        case "Lv100 ガルーダ":
+            var rec = '<tr class="selectable"><td>' + id + '</td></tr>>';
+            $('.garu-time').text(getDate());
+            $('#garu_tbl tbody > tr:last').after(rec);
+            break;
+        case "Lv100 アテナ":
+            var rec = '<tr class="selectable"><td>' + id + '</td></tr>>';
+            $('.ate-time').text(getDate());
+            $('#ate_tbl tbody > tr:last').after(rec);
+            break;
+        case "Lv100 グラニ":
+            var rec = '<tr class="selectable"><td>' + id + '</td></tr>>';
+            $('.gura-time').text(getDate());
+            $('#gura_tbl tbody > tr:last').after(rec);
+            break;
+        case "Lv100 バアル":
+            var rec = '<tr class="selectable"><td>' + id + '</td></tr>>';
+            $('.baa-time').text(getDate());
+            $('#baa_tbl tbody > tr:last').after(rec);
+            break;
+        case "Lv100 フラム＝グラス":
+            var rec = '<tr class="selectable"><td>' + id + '</td></tr>>';
+            $('.hura-time').text(getDate());
+            $('#hura_tbl tbody > tr:last').after(rec);
+            break;
+        case "Lv100 マキュラ・マリウス":
+            var rec = '<tr class="selectable"><td>' + id + '</td></tr>>';
+            $('.makyu-time').text(getDate());
+            $('#makyu_tbl tbody > tr:last').after(rec);
+            break;
+        case "Lv100 メドゥーサ":
+            var rec = '<tr class="selectable"><td>' + id + '</td></tr>>';
+            $('.medu-time').text(getDate());
+            $('#medu_tbl tbody > tr:last').after(rec);
+            break;
+        case "Lv100 ナタク":
+            var rec = '<tr class="selectable"><td>' + id + '</td></tr>>';
+            $('.nata-time').text(getDate());
+            $('#nata_tbl tbody > tr:last').after(rec);
+            break;
     }
 };
 /* テーブルデータがクリックされたら、クリップボードにコピーする */
@@ -96,6 +136,16 @@ $("#lv100_tab").on("click", function () {
     $("#shuva_body").addClass("hide");
     $("#yugu_body").addClass("hide");
     $("#tia_body").addClass("hide");
+
+    $("#garu_body").addClass("hide");
+    $("#ate_body").addClass("hide");
+    $("#baa_body").addClass("hide");
+    $("#gura_body").addClass("hide");
+
+    $("#hura_body").addClass("hide");
+    $("#makyu_body").addClass("hide");
+    $("#medu_body").addClass("hide");
+    $("#nata_body").addClass("hide");
 
     $("#grand-tag img").removeClass("unselect");
     $("#grand-tag span").removeClass("unselect");
@@ -130,6 +180,16 @@ $("#magna_tab").on("click", function () {
     $("#yugu_body").removeClass("hide");
     $("#tia_body").removeClass("hide");
 
+    $("#garu_body").addClass("hide");
+    $("#ate_body").addClass("hide");
+    $("#baa_body").addClass("hide");
+    $("#gura_body").addClass("hide");
+
+    $("#hura_body").addClass("hide");
+    $("#makyu_body").addClass("hide");
+    $("#medu_body").addClass("hide");
+    $("#nata_body").addClass("hide");
+
     $("#grand-tag img").addClass("unselect");
     $("#grand-tag span").addClass("unselect");
     $("#baha-tag img").addClass("unselect");
@@ -162,6 +222,16 @@ $("#all_tab").on("click", function () {
     $("#shuva_body").removeClass("hide");
     $("#yugu_body").removeClass("hide");
     $("#tia_body").removeClass("hide");
+
+    $("#garu_body").addClass("hide");
+    $("#ate_body").addClass("hide");
+    $("#baa_body").addClass("hide");
+    $("#gura_body").addClass("hide");
+
+    $("#hura_body").addClass("hide");
+    $("#makyu_body").addClass("hide");
+    $("#medu_body").addClass("hide");
+    $("#nata_body").addClass("hide");
 
     $("#grand-tag img").removeClass("unselect");
     $("#grand-tag span").removeClass("unselect");
@@ -196,6 +266,16 @@ $("#ang_tab").on("click", function () {
     $("#yugu_body").addClass("hide");
     $("#tia_body").addClass("hide");
 
+    $("#garu_body").addClass("hide");
+    $("#ate_body").addClass("hide");
+    $("#baa_body").addClass("hide");
+    $("#gura_body").addClass("hide");
+
+    $("#hura_body").addClass("hide");
+    $("#makyu_body").addClass("hide");
+    $("#medu_body").addClass("hide");
+    $("#nata_body").addClass("hide");
+
     $("#grand-tag img").addClass("unselect");
     $("#grand-tag span").addClass("unselect");
     $("#baha-tag img").addClass("unselect");
@@ -212,6 +292,99 @@ $("#ang_tab").on("click", function () {
     $("#yugu-tag span").addClass("unselect");
     $("#tia-tag img").addClass("unselect");
     $("#tia-tag span").addClass("unselect");
+
+
+});
+
+$("#new_tab").on("click", function () {
+    $("#baha_body").addClass("hide");
+    $("#gra_body").addClass("hide");
+
+    $("#rafa_body").addClass("hide");
+    $("#uri_body").addClass("hide");
+    $("#gabu_body").addClass("hide");
+    $("#mika_body").addClass("hide");
+
+    $("#riva_body").addClass("hide");
+    $("#coro_body").addClass("hide");
+    $("#sere_body").addClass("hide");
+    $("#shuva_body").addClass("hide");
+    $("#yugu_body").addClass("hide");
+    $("#tia_body").addClass("hide");
+
+    $("#garu_body").removeClass("hide");
+    $("#ate_body").removeClass("hide");
+    $("#baa_body").removeClass("hide");
+    $("#gura_body").removeClass("hide");
+
+    $("#hura_body").addClass("hide");
+    $("#makyu_body").addClass("hide");
+    $("#medu_body").addClass("hide");
+    $("#nata_body").addClass("hide");
+
+    $("#grand-tag img").addClass("unselect");
+    $("#grand-tag span").addClass("unselect");
+    $("#baha-tag img").addClass("unselect");
+    $("#baha-tag span").addClass("unselect");
+    $("#riva-tag img").addClass("unselect");
+    $("#riva-tag span").addClass("unselect");
+    $("#coro-tag img").addClass("unselect");
+    $("#coro-tag span").addClass("unselect");
+    $("#sere-tag img").addClass("unselect");
+    $("#sere-tag span").addClass("unselect");
+    $("#shuva-tag img").addClass("unselect");
+    $("#shuva-tag span").addClass("unselect");
+    $("#yugu-tag img").addClass("unselect");
+    $("#yugu-tag span").addClass("unselect");
+    $("#tia-tag img").addClass("unselect");
+    $("#tia-tag span").addClass("unselect");
+
+
+});
+$("#old_tab").on("click", function () {
+    $("#baha_body").addClass("hide");
+    $("#gra_body").addClass("hide");
+
+    $("#rafa_body").addClass("hide");
+    $("#uri_body").addClass("hide");
+    $("#gabu_body").addClass("hide");
+    $("#mika_body").addClass("hide");
+
+    $("#riva_body").addClass("hide");
+    $("#coro_body").addClass("hide");
+    $("#sere_body").addClass("hide");
+    $("#shuva_body").addClass("hide");
+    $("#yugu_body").addClass("hide");
+    $("#tia_body").addClass("hide");
+
+    $("#garu_body").addClass("hide");
+    $("#ate_body").addClass("hide");
+    $("#baa_body").addClass("hide");
+    $("#gura_body").addClass("hide");
+
+    $("#hura_body").removeClass("hide");
+    $("#makyu_body").removeClass("hide");
+    $("#medu_body").removeClass("hide");
+    $("#nata_body").removeClass("hide");
+
+    $("#grand-tag img").addClass("unselect");
+    $("#grand-tag span").addClass("unselect");
+    $("#baha-tag img").addClass("unselect");
+    $("#baha-tag span").addClass("unselect");
+    $("#riva-tag img").addClass("unselect");
+    $("#riva-tag span").addClass("unselect");
+    $("#coro-tag img").addClass("unselect");
+    $("#coro-tag span").addClass("unselect");
+    $("#sere-tag img").addClass("unselect");
+    $("#sere-tag span").addClass("unselect");
+    $("#shuva-tag img").addClass("unselect");
+    $("#shuva-tag span").addClass("unselect");
+    $("#yugu-tag img").addClass("unselect");
+    $("#yugu-tag span").addClass("unselect");
+    $("#tia-tag img").addClass("unselect");
+    $("#tia-tag span").addClass("unselect");
+
+
 });
 // $("#grand-tag").on("click", function () {
 //     if($("#grand-tag img").hasClass('unselect')){
